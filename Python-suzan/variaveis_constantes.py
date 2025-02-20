321f694cb2a241eefbb858f49840ba985f8df3ef
Ana Beatriz <anabeatrizbarbosasilva7@gmail.com>
2025-02-20 15:32:41 -0300

    Create variaveis_constantes.py

---
+++ Python-suzan/variaveis_constantes.py
@@ -0,0 +1,19 @@
+nome='Ana'
+idade=22
+print(nome,idade)
+
+nome, idade=('Ana Beatriz', 23)
+
+print(nome,idade)
+
+limite_saque_diario = 100
+
+STATES = ["SP","PE", "SC"] 
+
+preco = 10.5
+print(int(preco))
+texto = f"nome {nome} idade {idade}"
+print(type(preco))
+print(texto)
+
+print(5 // 2)
