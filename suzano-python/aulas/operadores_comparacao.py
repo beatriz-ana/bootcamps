1af5ea5e7c749380bd2e7cd8502a703bb4b76436
Ana Beatriz <anabeatrizbarbosasilva7@gmail.com>
2025-02-20 17:04:08 -0300

    Create operadores_comparacao.py

---
+++ suzano-python/aulas/operadores_comparacao.py
@@ -0,0 +1,10 @@
+# Aula sobre Operadores de Comparação
+saldo = 200
+saque = 200
+
+print(saldo == saque)
+print(saldo != saque)
+print(saldo > saque)
+print(saldo >= saque)
+print(saldo < saque)
+print(saldo <= saque)
