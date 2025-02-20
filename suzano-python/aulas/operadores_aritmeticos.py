6a0d28f52c53f14707381decf783dba6f8e583b4
Ana Beatriz <anabeatrizbarbosasilva7@gmail.com>
2025-02-20 16:07:24 -0300

    Create operadores_aritmeticos.py

---
+++ suzano-python/aulas/operadores_aritmeticos.py
@@ -0,0 +1,22 @@
+produto_1 = 20
+produto_2 = 10
+
+#print(f"Produto 1 =" {produto_1 } "Produto 2= " {produto_2})
+#soma
+print("soma= ", produto_1 + produto_2)
+#subtração
+print("subtração= ",  produto_1 - produto_2)
+#multiplicação
+print("multiplicação= ", produto_1 * produto_2)
+#divisão
+print("divisão= ", produto_1 / produto_2)
+#divisão inteira
+print("divisão inteira= ", produto_1 // produto_2)
+#Módulo- resto da divisão 
+print("Módulo= ", produto_1 % produto_2)
+#exponenciação
+print("Potência= ", produto_1 ** produto_2)
+
+#Ordem de precedência
+
+total = print("produto_1 + 22 +(produto_2*2) = " , produto_1+ 22 + (produto_2*2))
