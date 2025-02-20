5dadf471c96c00bc7ce16255f018765878aecf15
Ana Beatriz <anabeatrizbarbosasilva7@gmail.com>
2025-02-20 17:23:24 -0300

    Create operadores_associacao.py

---
+++ suzano-python/aulas/operadores_associacao.py
@@ -0,0 +1,15 @@
+# Aula sobre Operadores de Associação
+lista = ["Python", 'Academy', "Operadores", 'Condições']
+
+# Verifica se existe a string dentro da lista
+print('Python' in lista)  # Saída: True
+print('SQL' in lista)  # Saída: False
+
+# Verifica se não existe a string dentro da lista
+print('Python' not in lista)  # Saída: False
+print('SQL' not in lista) # Saída: True
+
+frutas = ["limão", "laranja"]
+
+print('limão' in frutas)# Saída: True
+print('limão' not in frutas)# Saída: False
