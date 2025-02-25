044c87eebdd90be8c9db1dbbad41b5d841c7e024
Ana Beatriz <anabeatrizbarbosasilva7@gmail.com>
2025-02-25 15:52:07 -0300

    Update estrutura_repeticao_while.py

--- suzano-python/aulas/estrutura_repeticao_while.py
+++ suzano-python/aulas/estrutura_repeticao_while.py
@@ -1,8 +1,9 @@
+#Aula sobre estruturas de repetição - WHILE
 opcao = -1
 
 while opcao != 0:
     opcao = int(input("[1] Sacar \n[2] Extrato \n[0] Sair \n: "))
-    
+
     if opcao == 1:
         print("Sacando...")
     elif opcao == 2:
