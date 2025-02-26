7daeb84a4cb156573874b98d28d3e72da4e6653b
Ana Beatriz <anabeatrizbarbosasilva7@gmail.com>
2025-02-26 16:07:19 -0300

    Create string_fatiamento.py

---
+++ suzano-python/aulas/string_fatiamento.py
@@ -0,0 +1,17 @@
+#Aula sobre string - fatiamento
+
+nome = "Ana Beatriz Barbosa da Silva"
+
+print(nome[0])
+
+print(nome[:4])
+
+print(nome[12:])
+
+print(nome[12:20])
+
+print(nome[12:20:2])
+
+print(nome[:])
+
+print(nome[::-1])
