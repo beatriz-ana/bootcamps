f821a4e0a8022ceb4c1528460a5002f931a04c5f
Ana Beatriz <anabeatrizbarbosasilva7@gmail.com>
2025-02-26 16:07:36 -0300

    Create string_triplas.py

---
+++ suzano-python/aulas/string_triplas.py
@@ -0,0 +1,30 @@
+#Aula sobre string multiplas linhas
+
+nome = "Ana"
+
+mensagem = f"""
+Olá meu nome é {nome},
+Eu estou aprendendo Python
+    Essa mensagem tem diferentes recuos.
+"""
+print(mensagem)
+
+print(
+    '''
+    ************** MENU **************
+
+    1 - Depositar
+    2 - Sacar
+    0 - Sair
+    
+    **********************************
+
+            Obrigado por usar nosso sistema!!
+    '''
+)
+
+menu = "************** MENU **************\n"
+menu = "\n"
+menu = "1 - Depositar"
+
+print(menu)
